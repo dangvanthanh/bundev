b382794c6521ad8be335929f54d71a7cdc3eb6f3
Dang Van Thanh <dangvanthanh@dangthanh.org>
2023-06-04 20:57:20 +0700

    Tests with bun

---
+++ server.ts
@@ -0,0 +1,18 @@
+const server = Bun.serve({
+	port: 3000,
+	fetch(req) {
+		const url = new URL(req.url);
+
+		if (url.pathname === "/") {
+			return new Response("Home page");
+		}
+
+		if (url.pathname === "/blog") {
+			return new Response("Blog page");
+		}
+
+		return new Response("404");
+	},
+});
+
+console.log(`Listening on http://localhost:${server.port} ...`);
--- server/index.ts
+++
@@ -1,8 +0,0 @@
-const server = Bun.serve({
-  port: 3000,
-  fetch() {
-    return new Response("Hello Bun!");
-  },
-});
-
-console.log(`Listening on http://localhost:${server.port} ...`);
