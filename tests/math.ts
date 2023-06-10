68c8d1e52dd3d4fec71c46fdfadc475fe35d0596
Dang Van Thanh <dangvanthanh@dangthanh.org>
2023-06-10 10:40:47 +0700

    Update tests

---
+++ tests/math.ts
@@ -0,0 +1,15 @@
+export function add(a: number, b: number) {
+  return a + b;
+}
+
+export function subtract(a: number, b: number) {
+  return a - b;
+}
+
+export function multiply(a: number, b: number) {
+  return a * b;
+}
+
+export function division(a: number, b: number) {
+  return a / b;
+}
