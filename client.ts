240d50b1f8d8cfbed39e84bdac04723a8a752c58
Dang Van Thanh <dangvanthanh@dangthanh.org>
2023-08-12 00:04:34 +0700

    Update monorepo in bun

--- client.ts
+++
@@ -1,8 +0,0 @@
-import { edenTreaty } from '@elysiajs/eden';
-import type { App } from './server';
-
-const app = edenTreaty<App>('http://localhost:8080');
-
-const { data: id, error } = app.id['1990'].get();
-
-console.log(id, error);
---
+++ packages/client/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { css } from "../../styled-system/css";
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1 :class="css({ fontSize: '5xl', fontWeight: 'bold' })">{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">Count is {{ count }}</button>
+  </div>
+</template>
+
+<style scoped>
+</style>
