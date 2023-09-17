12529320448ade772796d561d81f0cbc0c282230
Dang Van Thanh <dangvanthanh@dangthanh.org>
2023-09-17 12:38:52 +0700

    Add htmx and pandacss

--- README.md
+++ README.md
@@ -5,10 +5,13 @@
 ## Technologies
 
 - [Bun](https://bun.sh/)
+- [Elysia](https://elysiajs.com/)
+- [Htmx](https://htmx.org/)
+- [PandaCSS](https://panda-css.com/)
+- [Vercel AI SDK](https://sdk.vercel.ai/docs)
 - [Biome](https://biomejs.dev)
 - [Puppetter](https://pptr.dev/)
 - [Discord.js](https://discord.js.org/)
-- [Elysia](https://elysiajs.com/)
 
 ## License
 
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 import { cors } from '@elysiajs/cors';
 import { staticPlugin } from '@elysiajs/static';
-import { swagger } from '@elysiajs/swagger';
 import { Elysia } from 'elysia';
 
 import { books, web } from './plugins';
 
 const app = new Elysia();
 
-app.use(swagger()).use(cors()).use(staticPlugin()).use(web).use(books).listen(3000);
+app.use(cors()).use(staticPlugin()).use(web).use(books).listen(3000);
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
 
