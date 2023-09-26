29c3957bb4d56fe7fc428474c578e13cfeb70c45
Dang Van Thanh <dangvanthanh@dangthanh.org>
2023-09-26 22:18:56 +0700

    Remove files unnessessary

--- src/models/books.ts
+++
@@ -1,9 +0,0 @@
-import Elysia, { t } from 'elysia';
-
-export const bookModel = (app: Elysia) =>
-  app.model({
-    book: t.Object({
-      name: t.String(),
-      author: t.String(),
-    }),
-  });
